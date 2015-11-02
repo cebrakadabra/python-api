e7c9406909b8db075d0c7b65992b0f159687383a
Christoph Eberl <office@cebra-webdesign.at>
2015-11-02 23:43:00 +0100

    fixed bug - missed to delete @api_view

--- restapi/snippets/views.py
+++ restapi/snippets/views.py
@@ -27,7 +27,6 @@ class SnippetList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # method for showing a specific one (GET/:id) or update a specific one (PUT/:id)
-@api_view(['GET', 'PUT', 'DELETE'])
 class SnippetDetail(APIView):
     """
     Retrieve, update or delete a snippet instance.

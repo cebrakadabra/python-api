c084e3e28a2de049c20b2fa60a952de98c87a75a
Christoph Eberl <office@cebra-webdesign.at>
2015-11-02 23:31:08 +0100

    disable admin mode

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+#     'PAGE_SIZE': 10
+# }
 
 
 # Internationalization

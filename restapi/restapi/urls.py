87020a9f661d1d38de407950e2cc183c541bc331
Christoph Eberl <office@cebra-webdesign.at>
2015-11-03 01:43:34 +0100

    fixed bug for startup an app

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
-from restapi import views
 from snippets import views
 
 # Create a router and register our viewsets with it.
